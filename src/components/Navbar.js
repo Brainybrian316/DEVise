cffb27282be605f36dbed10a16fff15bd7fd7da2
Brainybrian316 <brainybrian316@gmail.com>
2022-07-17 18:18:14 -0400

    hamburger clickable

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,107 @@
-import { useState } from 'react';
-import { Menu } from 'semantic-ui-react';
+import { useEffect, useState } from 'react';
 // import  image from '../assets/images/DEVise-bg.png';
 import './navbar.css';
 
 
 export default function Navbar() {
-  const [activeItem, setActiveItem] = useState('home');
+ const [isOpen, setIsOpen] = useState(false);
 
-  const handleItemClick = (e, { name }) => setActiveItem(name);
+  useEffect(() => {
+    document.addEventListener('click', handleClick);
+    return () => {
+      document.removeEventListener('click', handleClick);
+    }
+  }, []);
+
+  // navbar open and close useEffect
+  const handleClick = useEffect(() => {
+    let navBtns = document.querySelectorAll('.nav-btn');
+    navBtns.forEach(btn => {
+        let dropDownMenu= document.getElementById('header-menu')
+        btn.classList.toggle('active');
+        dropDownMenu.classList.toggle('active');
+    
+    }
+    )
+  });
+
+    
+
+
+
 
   return (
-    <Menu stackable inverted>
-      <Menu.Item id="fancy-nav">
-        {/* <img src={image} id="nav-image" alt="devise" /> */}
-        <i class="fa-solid fa-d"></i>
-        <i class="fa-solid fa-e"></i>
-        <i class="fa-solid fa-v"></i>
-        <i class="fa-solid fa-binoculars"></i>
-        <i class="fa-solid fa-z"></i>
-      </Menu.Item>
+    <div className="header header-fixed u-unselectable header-animated">
+    <div className="header-brand">
+        <div className="nav-item no-hover" id="fancy-nav">
+        <i className="fa-solid fa-d"></i>
+        <i className="fa-solid fa-e"></i>
+        <i className="fa-solid fa-v"></i>
+        <i className="fa-solid fa-binoculars"></i>
+        <i className="fa-solid fa-z"></i>
+        </div>
+        <div className="nav-item nav-btn " id="header-btn" 
+        onClick={() => setIsOpen(!isOpen)}
+        >
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <div className="header-nav" id="header-menu"
+     onClick={handleClick}
+    >
+        <div className="nav-left">
+            <div className="nav-item text-center">
+                <a href="#">
+                    <span className="icon">
+                        <i className="fab fa-wrapper fa-github" aria-hidden="true"></i>
+                    </span>
+                </a>
+            </div>
+            <div className="nav-item text-center">
+                <a href="#">
+                    <span className="icon">
+                        <i className="fab fa-wrapper fa-slack" aria-hidden="true"></i>
+                    </span>
+                </a>
+            </div>
+            <div className="nav-item text-center">
+                <a href="#">
+                    <span className="icon">
+                        <i className="fab fa-wrapper fa-twitter" aria-hidden="true"></i>
+                    </span>
+                </a>
+            </div>
+            <div className="nav-item has-sub toggle-hover" id="dropdown">
+                <a className="nav-dropdown-link">Animated</a>
+                <ul className="dropdown-menu dropdown-animated" role="menu">
+                    <li role="menu-item"><a href="#">First Item</a></li>
+                    <li role="menu-item"><a href="#">Second Item</a></li>
+                    <li role="menu-item"><a href="#">Third Item</a></li>
+                </ul>
+            </div>
+        </div>
 
-      <Menu.Item name="home" active={activeItem === 'home'} onClick={handleItemClick} color="red">
-        Home
-      </Menu.Item>
-
-      <Menu.Item name="ideaBin" active={activeItem === 'ideaBin'} onClick={handleItemClick} color="blue">
-        Idea Bin
-      </Menu.Item>
-
-      <Menu.Item name="projectFeed" active={activeItem === 'projectFeed'} onClick={handleItemClick} color="green">
-        ProjectFeed
-      </Menu.Item>
-
-      <Menu.Item name="login" active={activeItem === 'login'} onClick={handleItemClick} color="orange">
-        Login
-      </Menu.Item>
-
-      <Menu.Item name="signup" active={activeItem === 'signup'} onClick={handleItemClick} color="purple">
-        Signup
-      </Menu.Item>
-    </Menu>
+        <div className="nav-right">
+            <div className="nav-item active">
+                <a href="#">Active</a>
+            </div>
+            <div className="nav-item">
+                <a href="#">Link 1</a>
+            </div>
+            <div className="nav-item has-sub" id="dropdown">
+                <a className="nav-dropdown-link">Click Me</a>
+                <ul className="dropdown-menu" role="menu">
+                    <li role="menu-item"><a href="#">First Item</a></li>
+                    <li role="menu-item"><a href="#">Second Item</a></li>
+                    <li role="menu-item"><a href="#">Third Item</a></li>
+                    <li className="dropdown-menu-divider"></li>
+                    <li role="menu-item"><a href="#">Fourth Item</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
   );
 }
