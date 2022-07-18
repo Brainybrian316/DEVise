46467e4c6e9b317d2778426d0da2ac6af01973f2
Brainybrian316 <brainybrian316@gmail.com>
2022-07-18 00:01:07 -0400

    seeds index.js

--- src/services/seeds/index.js
+++ src/services/seeds/index.js
@@ -1,8 +1,12 @@
 const userSeeds = require('./userSeeds');
+const devProjectSeeds = require('./DevProjectSeeds');
 
 
 async function allSeeds() {
   await userSeeds();
+  console.log('seeded users');
+  await devProjectSeeds();
+  console.log('Seeded dev projects');
   console.log('Seeds complete');
 }
 
