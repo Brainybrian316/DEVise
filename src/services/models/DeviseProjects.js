31e3b972b67bb03e5a0232ff4fbab1bce1537c55
Brainybrian316 <brainybrian316@gmail.com>
2022-07-19 14:43:23 -0400

    refactored devprojects model to be more clean

--- src/services/models/DeviseProjects.js
+++ src/services/models/DeviseProjects.js
@@ -15,33 +15,25 @@ const deviseProjectsSchema = new Schema({
     type: String,
   },
   //for now the authors are DEVise. Eventually it will include all contributors to a template similar to an nom package.
-  authors: [
-    {
-      type: String,
+  authors: {
+      type: Array, 
     },
-  ],
   //main image will be required
   mainImage: {
     type: String,
   },
   //images applies to subsidiary images - these are optional
-  images: [
-    {
-      type: String,
+  images: {
+      type: Array,
     },
-  ],
   //videos will not be required
-  videos: [
-    {
-      type: String,
+  videos:{
+      type: Array,
     },
-  ],
   //tags are a potential future feature. For now, they are just a string array
-  tags: [
-    {
-      type: String,
+  tags: {
+      type: Array,
     },
-  ],
   createdAt: {
     type: Date,
     default: Date.now,
