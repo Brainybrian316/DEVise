d7775463631a8f583ea1ab9cb36080d06fd7b378
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-19 12:42:55 -0400

    Add user, name, image schema req

--- src/services/models/Projects.js
+++ src/services/models/Projects.js
@@ -1,7 +1,26 @@
 import mongoose from 'mongoose';
 
+const productSchema = mongoose.Schema(
+    {
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			required: true,
+			ref: 'User',
+		},
+		name: {
+			type: String,
+			required: true,
+		},
+		image: {
+			type: String,
+			required: true,
 
-
+        },
+        {
+            timestamps: true,
+        }
+    );
+    
 const Projects = mongoose.model('Projects', projectsSchema);
 
 module.exports = Projects;
