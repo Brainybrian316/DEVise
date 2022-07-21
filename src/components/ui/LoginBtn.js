b279a6ad7efdcf0d587c36e0000a8bad07cd53f2
Brainybrian316 <brainybrian316@gmail.com>
2022-07-20 23:33:43 -0400

    login button

---
+++ src/components/ui/LoginBtn.js
@@ -0,0 +1,22 @@
+import Button from '@mui/material/Button';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
+
+
+export default function loginBtn() {
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#e91e63',
+      },
+      
+    }
+    });
+  return (
+    <ThemeProvider theme={theme}>
+    <Button onClick={() => {alert('clicked');
+  }} variant="contained">Login</Button>
+    </ThemeProvider>
+  );
+}
