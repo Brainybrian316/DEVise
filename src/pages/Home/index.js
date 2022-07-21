74634db9a85467fdea4df431f1cd52ac8aa04467
Brainybrian316 <brainybrian316@gmail.com>
2022-07-20 23:49:28 -0400

    signup button

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,12 @@
-
+import SignupBtn from "../../components/ui/SignupBtn"
 
 export default function Home() {
   return (
     <>
       
       <h1>Home</h1>
+      <SignupBtn />
 
- 
     </>
   )
 }
