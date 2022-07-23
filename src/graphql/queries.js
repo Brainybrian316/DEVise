30dae34b7c2b7d2dcf4f5afd257dec384d985fe7
Brainybrian316 <brainybrian316@gmail.com>
2022-07-22 23:51:10 -0400

    user queries

---
+++ src/graphql/queries.js
@@ -0,0 +1,54 @@
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client';
+
+
+const GRAPH_URL = 'http://localhost:9000/graphql';
+
+export const client = new ApolloClient({
+  uri: GRAPH_URL,
+  cache: new InMemoryCache(),
+});
+
+export const USER_PROJECTS_FRAGMENT = gql`
+  fragment userProjects on User {
+    id
+    title
+    description
+    summary
+    contributors
+    mainImage
+    images
+    videos
+    tags
+    rating
+    numReviews
+    price
+  }
+`;
+
+export const GET_USER_QUERY = gql`
+  query GetUser($id: ID!) {
+    user(id: $id) {
+      id
+      firstName
+      lastName
+      username
+      email
+      password
+      ${USER_PROJECTS_FRAGMENT}
+    }
+  }
+`;
+
+export const GET_USERS_QUERY = gql`
+  query GetUsers {
+    users {
+      id
+      firstName
+      lastName
+      username
+      email
+      password
+    }
+  }
+`;
+
