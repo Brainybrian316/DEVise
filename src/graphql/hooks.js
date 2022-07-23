d9f2ac7f517fa23a46b5eec2d57f777a5081f245
Brainybrian316 <brainybrian316@gmail.com>
2022-07-23 12:55:33 -0400

    get me hook

--- src/graphql/hooks.js
+++ src/graphql/hooks.js
@@ -1,4 +1,4 @@
-import {GET_USER_QUERY,   GET_USERS_QUERY , USER_PROJECTS_FRAGMENT, GET_USER_PROJECTS_QUERY, GET_ALL_USER_PROJECTS_QUERY  } from "./queries";
+import {GET_USER_QUERY,   GET_USERS_QUERY , USER_PROJECTS_FRAGMENT, GET_USER_PROJECTS_QUERY, GET_ALL_USER_PROJECTS_QUERY, GET_ME_QUERY  } from "./queries";
 import { useMutation, useQuery } from "@apollo/client";
 
 export function useGetUserQuery(id) {
@@ -39,4 +39,14 @@ export function useGetAllUserProjectsQuery() {
     loading,
     error: Boolean(error),
   }
+}
+
+export function useGetMeQuery() {
+  const { data, loading, error } = useQuery
+  (GET_ME_QUERY);
+  return {
+    me: data?.me,
+    loading,
+    error: Boolean(error),
+}
 }
