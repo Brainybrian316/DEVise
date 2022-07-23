33d88eb7f66c09e693d6d45fb677ab47b9ae9fb2
Brainybrian316 <brainybrian316@gmail.com>
2022-07-23 01:04:04 -0400

    user and users display properly

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,11 @@
-import { useGetUserQuery } from "../../graphql/hooks.js";
+
 
 
 export default function Home() {
-
-  const { user, loading, error } = useGetUserQuery(
-    "2db3d15f265086c76a138f2"
-  );
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-
   return (
     <>
 
       <h1>Home</h1>
-      <p>Welcome {user.firstName} {user.lastName}</p>
-      <p>Your username is {user.username}</p>
-      <p>Your email is {user.email}</p>
-      <p>Your password is {user.password}</p>
-      <p>Your projects are:</p>
-      
-    
 
 
    </>
