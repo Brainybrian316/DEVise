638205a9eed1ba15c7c8d2074a4909745ff0d7d1
Brainybrian316 <brainybrian316@gmail.com>
2022-07-23 12:46:24 -0400

    get all user projects hook

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,31 +1,17 @@
-import { useGetUserProjectsQuery, } from "../../graphql/hooks"
-
 
 export default function Home() {
 
-  const { loading, error, userProject } = useGetUserProjectsQuery('62db3d006c3c25609f5a9ff6');
+ 
 
 
-  if (loading) return <p>...Loading</p>
-  if (error) return <p>Something went wrong</p>
+  // if (loading) return <p>...Loading</p>
+  // if (error) return <p>Something went wrong</p>
 
   return (
     <>
 
       <h1>Home</h1>
-      <p>{userProject.title}</p>
-      <p>{userProject.description}</p>
-      <p>{userProject.summary}</p>
-      <p>{userProject.contributors}</p>
-      <p>{userProject.mainImage}</p>
-      <p>{userProject.images}</p>
-      <p>{userProject.videos}</p>
-      <p>{userProject.tags}</p>
-      <p>{userProject.rating}</p>
-      <p>{userProject.numReviews}</p>
-      <p>{userProject.price}</p>
-      
-
+    
 
    </>
     
