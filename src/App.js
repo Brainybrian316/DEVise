18622e23bda65789174a76f1a33807631a412ca2
Brainybrian316 <brainybrian316@gmail.com>
2022-07-23 19:38:49 -0400

    login

--- src/App.js
+++ src/App.js
@@ -2,52 +2,20 @@
 import HomePage from './pages/Home';
 import './assets/global.css';
 import Navbar from './components/Navbar';
-import { useState } from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { client } from './graphql/queries';
+import Login from './pages/Login';
 
 
 
 
 function App() {
-
-  const [getPage, setPage] = useState('')
-
-  function renderPage(page) {
-    switch(page){
-      case '': return <HomePage />;
-      // case 'About Us': return <AboutUs />;
-      // case 'checkout': return <Checkout />;
-      // case 'Contact': return <Contact />;
-      // case 'devconnect': return <DevConnect />;
-      // case 'membership': return <Membership />;
-      // case 'Profile': return <Profile />;
-      // case 'Projects': return <ProjectFeed />;
-      // case 'login': return <Login />
-      // case 'signup': return <Signup />
-      case 'login': {
-        alert('login button clicked')
-        break;
-      };
-      case 'signup': {
-        alert('signup button clicked')
-        break;
-      }
-      default: return <HomePage />
-    }
-  }
+  
   return (
     <ApolloProvider client={client}>
-    <div>    
-    <Navbar 
-    setPage={setPage}
-    />
-    <main>
-    {renderPage(getPage)}
-    </main>
-
-    
-    </div>
+    <Navbar /> 
+    <Login/>
+    <HomePage />
     </ApolloProvider>
   );
 }
