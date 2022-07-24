099093f6c2f6f48ea233b2f16f586ed20c9d8ddb
Brainybrian316 <brainybrian316@gmail.com>
2022-07-24 13:58:39 -0400

    working on createing user

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -12,6 +12,7 @@ import {
 import { createTheme } from '@mui/material/styles';
 import { useCreateUserMutation } from '../../hooks/Mutations';
 
+<<<<<<< HEAD
 export default function Signup() {
   const {createUser} = useCreateUserMutation();
   const [firstName, setFirstName] = useState('');
@@ -19,6 +20,15 @@ export default function Signup() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+=======
+
+import './signup.css';
+import { Container, Box, Typography } from '@mui/material';
+import CreateAcctInput from './CreateInput';
+import Button from '@mui/material/Button';
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
+>>>>>>> 7c822fd (working on createing user)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -68,6 +78,7 @@ export default function Signup() {
     },
   });
 
+<<<<<<< HEAD
   return (
     <div>
       <Container maxWidth="sm">
@@ -226,6 +237,62 @@ export default function Signup() {
             </ThemeProvider>
           </Box>
         </Box>
+=======
+export default function Signup() {
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#3f51b5',
+      },
+      
+    }
+    });
+  return (
+    <div>
+      <Container maxWidth="sm">
+      <Box sx={{ 
+        bgcolor: '#cfe8fc', 
+        height: 'auto',
+        borderRadius: '10px',
+        padding: '2rem',
+         }}>
+
+        <Typography variant="h4" sx={{
+          textAlign: 'start',
+          fontWeight: 'bold',
+          color: '#3f51b5',
+          fontSize: '2rem',
+          marginBottom: '2rem',
+        }}>
+          Create Account
+        </Typography>
+
+        <Box sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          marginBottom: '2rem',
+        }}>
+      <CreateAcctInput/>
+      </Box>
+
+        <Box sx={{
+          display: 'flex',
+          alignItems: 'right',
+          justifyContent: 'flex-end',
+        }}>
+      <ThemeProvider theme={theme}>
+      <Button 
+      id="form-btn"
+      onClick={() => {alert('clicked');
+    }} variant="contained">Create Account</Button>
+      </ThemeProvider>
+      </Box>
+
+      </Box>
+>>>>>>> 7c822fd (working on createing user)
       </Container>
     </div>
   );
