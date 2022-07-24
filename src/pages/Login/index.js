274489151d7e04b4bc6120da041f08004a4d97d9
Brainybrian316 <brainybrian316@gmail.com>
2022-07-24 13:20:30 -0400

    form styling

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -4,12 +4,9 @@ import { ThemeProvider } from '@mui/material/styles';
 import { useLoginMutation } from '../../hooks/Mutations';
 import { useState } from 'react';
 import { Container, Box, Typography } from '@mui/material';
-import {  FormGroup, InputLabel, Input, FormHelperText } from '@mui/material';
-
+import { FormControl, InputLabel, Input, FormHelperText } from '@mui/material';
 
 export default function Login() {
-
-
   const { login } = useLoginMutation();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -17,103 +14,139 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-    const userLoggedIn = await login({ variables: { email, password } });
-    if(userLoggedIn) {
-      alert('You are logged in');
-      console.log(userLoggedIn);
-    }
+      const userLoggedIn = await login({ variables: { email, password } });
+      if (userLoggedIn) {
+        alert('You are logged in');
+        console.log(userLoggedIn);
+      }
     } catch (error) {
       alert('invalid email or password');
       console.log(error);
     }
-  }
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#3f51b5',
-      },
-    }
-    });
+  };
 
-    const inputTheme = createTheme({
-      palette: {
-        primary: 
-        {
-          main: '#FFFFFF',
-        },
-      }
-      });
 
   return (
     <div>
       <Container maxWidth="sm">
-      <Box sx={{ 
-        bgcolor: '#cfe8fc', 
-        height: 'auto',
-        borderRadius: '10px',
-        padding: '2rem',
-         }}>
-
-      <Typography variant="h4" sx={{
-          textAlign: 'start',
-          fontWeight: 'bold',
-          color: '#3f51b5',
-          fontSize: '2rem',
-          marginBottom: '2rem',
-        }}>
-          Log In Account
-        </Typography>
+        <Box
+          sx={{
+            bgcolor: '#cfe8fc',
+            height: 'auto',
+            borderRadius: '10px',
+            padding: '2rem',
+          }}
+        >
+          <Typography
+            variant="h4"
+            sx={{
+              textAlign: 'start',
+              fontWeight: 'bold',
+              color: '#3f51b5',
+              fontSize: '2rem',
+              marginBottom: '2rem',
+            }}
+          >
+            Log In Account
+          </Typography>
 
           {/* LOGIN form  */}
 
-          <Box sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginBottom: '2rem',
-          }}>
-             <ThemeProvider theme={inputTheme}>
-            <FormGroup>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center',
+              marginBottom: '2rem',
+            }}
+          >
+       
+            <FormControl
+              sx={{
+                width: '100%',
+                marginBottom: '1rem',
+              }}
+            >
+              <InputLabel htmlFor="email">Email address:</InputLabel>
+              <Input 
+              sx={{
+                bgcolor: '#DEDEDE',
+                borderRadius: '10px',
+              }}
+              id="email" aria-describedby="my-helper-text" />
+              <FormHelperText id="my-helper-text">youremail@yourdomain.com</FormHelperText>
+            </FormControl>
 
-  <InputLabel htmlFor="email">Email address:</InputLabel>
-  <Input id="email" 
-  value={email}
-  onChange={(e) => setEmail(e.target.value)}
-  type="email"
-  required
-  aria-describedby="my-helper-text" />
-  <FormHelperText id="my-helper-text">youremail@yourdomain.com</FormHelperText>
-
-  <InputLabel htmlFor="password">Password:</InputLabel>
-  <Input id="password" 
-  value={password}
-  onChange={(e) => setPassword(e.target.value)}
-  type="password"
-  required
-  aria-describedby="my-helper-text" />
-  <FormHelperText id="my-helper-text">At least 8 characters and one number</FormHelperText>
-
-</FormGroup>
-</ThemeProvider>
-
-      </Box>
-              
-          <Box sx={{
-            display: 'flex',
-            alignItems: 'right',
-            justifyContent: 'flex-end',
-          }}>
-        <ThemeProvider theme={theme}>
-      <Button 
-      id="form-btn"
-      onClick={handleSubmit}>Log In</Button>
-      </ThemeProvider>
-
+            <FormControl
+              sx={{
+                width: '100%',
+              }}
+            >
+              <InputLabel htmlFor="password">Password:</InputLabel>
+              <Input 
+              sx={{
+                bgcolor: '#DEDEDE',
+                borderRadius: '10px',
+              }}
+              id="password" aria-describedby="my-helper-text" />
+              <FormHelperText id="my-helper-text">8 characters and at least one number</FormHelperText>
+            </FormControl>
+            
+           
           </Box>
-          
-      </Box>
+
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'right',
+              justifyContent: 'flex-end',
+            }}
+          >
+             <Button id="form-btn"
+             sx={{
+              bgcolor: '#3f51b5',
+              borderRadius: '10px',
+              color: '#fff',
+              fontSize: '1rem',
+              fontWeight: 'bold',
+              '&:hover': {
+                color: '#47E9DD',
+              },
+             }}
+             onClick={handleSubmit}
+            >
+                Log In
+              </Button>
+          </Box>
+        </Box>
       </Container>
     </div>
-  )
+  );
+}
+
+{
+  /* <ThemeProvider theme={inputTheme}>
+<FormGroup>
+
+<InputLabel htmlFor="email">Email address:</InputLabel>
+<Input id="email" 
+value={email}
+onChange={(e) => setEmail(e.target.value)}
+type="email"
+required
+aria-describedby="my-helper-text" />
+<FormHelperText id="my-helper-text">youremail@yourdomain.com</FormHelperText>
+
+<InputLabel htmlFor="password">Password:</InputLabel>
+<Input id="password" 
+value={password}
+onChange={(e) => setPassword(e.target.value)}
+type="password"
+required
+aria-describedby="my-helper-text" />
+<FormHelperText id="my-helper-text">At least 8 characters and one number</FormHelperText>
+
+</FormGroup>
+</ThemeProvider> */
 }
