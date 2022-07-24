0069d73e8e7acc00b939ba13f2623748484af3d4
DeNiro Dumas <denirod@Deniros-MacBook-Pro.local>
2022-07-24 16:17:22 -0400

    member card

---
+++ src/pages/Membership/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MemberCard from "./MemberCard";
+
+function Membership() {
+  return (
+    <div>
+      <MemberCard />
+    </div>
+  );
+}
+
+export default Membership;
