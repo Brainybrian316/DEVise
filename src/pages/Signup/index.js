a9fcb84f6baa1828f2e5a89a5bb71b1565abb724
Brainybrian316 <brainybrian316@gmail.com>
2022-07-24 15:26:08 -0400

    rebase

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,71 +1,205 @@
-
-
-import './signup.css';
-import { Container, Box, Typography } from '@mui/material';
-import CreateAcctInput from './CreateInput';
-import Button from '@mui/material/Button';
-import { createTheme } from '@mui/material/styles';
-import { ThemeProvider } from '@mui/material/styles';
-import { useCreateUserMutation } from '../../hooks/Mutations';
 import { useState } from 'react';
+import {
+  InputLabel,
+  Input,
+  Box,
+  FormControl,
+  Button,
+  Container,
+  ThemeProvider,
+  Typography,
+} from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+// import { useCreateUserMutation } from '../../hooks/Mutations';
 
+export default function Signup() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // const [createUser, { loading, error }] = useCreateUserMutation();
 
-export default function Signup(props) {
- 
-  
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault();
+  //   const { data } = await createUser({
+  //     variables: {
+  //       firstName,
+  //       lastName,
+  //       username,
+  //       email,
+  //       password,
+  //     },
+  //   });
+  //   console.log(data);
+  // }
 
   const theme = createTheme({
     palette: {
       primary: {
         main: '#3f51b5',
       },
-      
-    }
-    });
+    },
+  });
+
   return (
     <div>
       <Container maxWidth="sm">
-      <Box sx={{ 
-        bgcolor: '#cfe8fc', 
-        height: 'auto',
-        borderRadius: '10px',
-        padding: '2rem',
-         }}>
+        <Box
+          sx={{
+            bgcolor: '#cfe8fc',
+            height: 'auto',
+            borderRadius: '10px',
+            padding: '2rem',
+          }}
+        >
+          <Typography
+            variant="h4"
+            sx={{
+              textAlign: 'start',
+              fontWeight: 'bold',
+              color: '#3f51b5',
+              fontSize: '2rem',
+              marginBottom: '1rem',
+            }}
+          >
+            Create Account
+          </Typography>
 
-        <Typography variant="h4" sx={{
-          textAlign: 'start',
-          fontWeight: 'bold',
-          color: '#3f51b5',
-          fontSize: '2rem',
-          marginBottom: '2rem',
-        }}>
-          Create Account
-        </Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}
+          >
+            <FormControl
+              sx={{
+                width: '45%',
+                marginRight: '1rem',
+                marginBottom: '1rem',
+              }}
+            >
+              <InputLabel htmlFor="firstName">First Name</InputLabel>
+              <Input
+                sx={{
+                  borderRadius: '10px',
+                  fontWeight: 'bold',
+                  color: '#3f51b5',
+                  backgroundColor: '#D8D8D8',
+                }}
+                id="firstName"
+                type="text"
+                onChange={(e) => setFirstName(e.target.value)}
+              />
+            </FormControl>
 
-        <Box sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          marginBottom: '2rem',
-        }}>
-      <CreateAcctInput/>
-      </Box>
+            <FormControl
+              sx={{
+                width: '45%',
+              }}
+            >
+              <InputLabel htmlFor="lastName">Last Name</InputLabel>
+              <Input
+                sx={{
+                  borderRadius: '10px',
+                  fontWeight: 'bold',
+                  color: '#3f51b5',
+                  backgroundColor: '#D8D8D8',
+                }}
+                id="lastName"
+                type="text"
+                onChange={(e) => setLastName(e.target.value)}
+              />
+            </FormControl>
+          </Box>
 
-        <Box sx={{
-          display: 'flex',
-          alignItems: 'right',
-          justifyContent: 'flex-end',
-        }}>
-      <ThemeProvider theme={theme}>
-      <Button 
-      id="form-btn"
-      onClick={() => {alert('clicked');
-    }} variant="contained">Create Account</Button>
-      </ThemeProvider>
-      </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}
+          >
+            <FormControl
+              sx={{
+                width: '100%',
+                marginBottom: '1rem',
+              }}
+            >
+              <InputLabel htmlFor="username">Username</InputLabel>
+              <Input
+                sx={{
+                  borderRadius: '10px',
+                  fontWeight: 'bold',
+                  color: '#3f51b5',
+                  backgroundColor: '#D8D8D8',
+                }}
+                id="username"
+                type="text"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </FormControl>
+          </Box>
 
-      </Box>
+          <FormControl
+            sx={{
+              width: '100%',
+              marginBottom: '1rem',
+            }}
+          >
+            <InputLabel htmlFor="email">Email</InputLabel>
+            <Input
+              sx={{
+                borderRadius: '10px',
+                fontWeight: 'bold',
+                color: '#3f51b5',
+                backgroundColor: '#D8D8D8',
+              }}
+              id="email"
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </FormControl>
+
+          <FormControl
+            sx={{
+              width: '100%',
+            }}
+          >
+            <InputLabel htmlFor="password">Password</InputLabel>
+            <Input
+              sx={{
+                borderRadius: '10px',
+                fontWeight: 'bold',
+                color: '#3f51b5',
+                backgroundColor: '#D8D8D8',
+              }}
+              id="password"
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </FormControl>
+
+          <Box
+            sx={{
+              paddingTop: '2em',
+              display: 'flex',
+              alignItems: 'right',
+              justifyContent: 'flex-end',
+            }}
+          >
+            <ThemeProvider theme={theme}>
+              <Button
+                id="form-btn"
+                variant="contained"
+                //  onClick={() => { handleSubmit() }}
+              >
+                Create Account
+              </Button>
+            </ThemeProvider>
+          </Box>
+        </Box>
       </Container>
     </div>
   );
