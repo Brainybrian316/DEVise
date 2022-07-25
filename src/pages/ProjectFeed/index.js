e7014e29bf7f482d83d20297d6c85de46f602b3e
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-25 16:56:01 -0400

    Add hero and searchbox

--- src/pages/ProjectFeed/index.js
+++ src/pages/ProjectFeed/index.js
@@ -2,14 +2,111 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import { useTheme } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import InputAdornment from '@mui/material/InputAdornment';
+import Chip from '@mui/material/Chip';
+
+import Container from './Container';
+import SearchBox from './SearchBox';
 
 
 const ProjectFeed = () => {
     const theme = useTheme();
     return (
-<>
-<h1>Example Text</h1>
-</>
+        <Box
+        position={'relative'}
+        sx={{
+          backgroundImage:
+            'url("https://assets.maccarianagency.com/backgrounds/img1.jpg")',
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          marginTop: -13,
+          paddingTop: 13,
+          '&:after': {
+            position: 'absolute',
+            content: '" "',
+            width: '100%',
+            height: '100%',
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+            zIndex: 1,
+            background: '#161c2d',
+            opacity: 0.6,
+          },
+        }}
+      >
+        <Container
+          zIndex={3}
+          position={'relative'}
+          minHeight={{ xs: 300, sm: 400, md: 600 }}
+          maxHeight={600}
+          display={'flex'}
+          alignItems={'center'}
+          justifyContent={'center'}
+        >
+          <Box>
+            <Box marginBottom={2}>
+              <Typography
+                variant="h2"
+                align={'center'}
+                sx={{
+                  fontWeight: 700,
+                  color: theme.palette.common.white,
+                }}
+              >
+                Project Feed
+              </Typography>
+            </Box>
+            <Box>
+              <Typography
+                variant="h6"
+                align={'center'}
+                sx={{
+                  color: theme.palette.common.white,
+                }}
+              >
+                Check out the latest happenings
+              </Typography>
+            </Box>
+          </Box>
+        </Container>
+        <Box
+          component={'svg'}
+          preserveAspectRatio="none"
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          viewBox="0 0 1920 100.1"
+          width={1}
+          maxHeight={120}
+          bottom={0}
+          position={'absolute'}
+          zIndex={2}
+        >
+          <path
+            d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
+          ></path>
+        </Box>
+        <Container
+          sx={{
+            marginTop: '-5rem',
+            position: 'relative',
+            zIndex: 3,
+            paddingY: '0 !important',
+          }}
+        >
+          <SearchBox />
+        </Container>
+      </Box>
+      
+
+      
+
+      
 
     );
   };
