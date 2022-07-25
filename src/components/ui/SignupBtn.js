9dc647276aab862e58d6fe465c6506644bb34202
Brainybrian316 <brainybrian316@gmail.com>
2022-07-24 22:09:53 -0400

    signup button redirects to signup page

--- src/components/ui/SignupBtn.js
+++ src/components/ui/SignupBtn.js
@@ -1,24 +1,23 @@
 import Button from '@mui/material/Button';
 import { createTheme } from '@mui/material/styles';
 import { ThemeProvider } from '@mui/material/styles';
-
+import { Link } from 'react-router-dom';
 
 export default function signupBtn(props) {
-  const { setPage } = props;
   const theme = createTheme({
     palette: {
       primary: {
         main: '#33d375',
       },
-      
-    }
-    });
+    },
+  });
   return (
+    <Link to="/signup">
     <ThemeProvider theme={theme}>
-    <Button 
-    sx={{ ml: 1, }}
-    onClick={() => {setPage('signup');
-  }} variant="contained"  id="fancy-nav"><span>Sign Up</span></Button>
+      <Button sx={{ ml: 1 }} variant="contained" id="fancy-nav">
+        <span>Sign Up</span>
+      </Button>
     </ThemeProvider>
+    </Link>
   );
 }
