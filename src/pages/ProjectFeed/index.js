537d2d8f67d6732f182f9973d2af6434b0d4f074
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-25 17:19:56 -0400

    Add card boxes and bottom field

--- src/pages/ProjectFeed/index.js
+++ src/pages/ProjectFeed/index.js
@@ -12,6 +12,9 @@ import Container from './Container';
 import SearchBox from './SearchBox';
 import FeaturedArticle from './FeaturedArticle';
 import LastStories from './LastStories';
+import CaseStudies from './CaseStudies';
+import PopularArticles from './PopularArticles';
+import Newsletter from './Newsletter';
 
 
 const ProjectFeed = () => {
@@ -109,6 +112,39 @@ const ProjectFeed = () => {
         <Container paddingTop={'0 !important'}>
           <LastStories />
         </Container>
+        <Container paddingTop={'0 !important'}>
+          <CaseStudies />
+        </Container>
+        <Box paddingBottom={{ xs: 2, sm: 3, md: 4 }}>
+          <Container paddingTop={'0 !important'}>
+            <PopularArticles />
+          </Container>
+        </Box>
+        <Box
+          component={'svg'}
+          preserveAspectRatio="none"
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          viewBox="0 0 1920 100.1"
+          sx={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            transform: 'translateY(50%)',
+            zIndex: 2,
+            width: 1,
+          }}
+        >
+          <path
+            fill={theme.palette.background.paper}
+            d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
+          ></path>
+        </Box>
+        <Container>
+        <Newsletter />
+      </Container>
       </Box>
       
 
