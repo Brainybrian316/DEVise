7024b9ed3975d40d39fdf4865aeed5749ae52937
Brainybrian316 <brainybrian316@gmail.com>
2022-07-25 17:58:43 -0400

    db for developers and about section

---
+++ src/pages/AboutUs/devDB.js
@@ -0,0 +1,49 @@
+
+
+export const devDB = [
+  {
+    name: 'Brian M',
+    title: 'Software Engineer',
+    github: require('../../assets/github.png'),
+    linkedin: require('../../assets/linkedin.png'),
+    email: require('../../assets/email.png'),
+    image: require('../../assets/brian.jpg'),
+    bio: 'Brian is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
+  },
+  {
+    name: 'Brian M',
+    title: 'Software Engineer', 
+    github: require('../../assets/github.png'),
+    linkedin: require('../../assets/linkedin.png'),
+    email: require('../../assets/email.png'),
+    image: require('../../assets/brian.jpg'),
+    bio: 'Brian is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
+  },
+  {
+    name: 'Brian M',
+    title: 'Software Engineer', 
+    github: require('../../assets/github.png'),
+    linkedin: require('../../assets/linkedin.png'),
+    email: require('../../assets/email.png'),
+    image: require('../../assets/brian.jpg'),
+    bio: 'Brian is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
+  },
+  {
+    name: 'Brian M',
+    title: 'Software Engineer', 
+    github: require('../../assets/github.png'),
+    linkedin: require('../../assets/linkedin.png'),
+    email: require('../../assets/email.png'),
+    image: require('../../assets/brian.jpg'),
+    bio: 'Brian is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
+  },
+  {
+    name: 'Brian M',
+    title: 'Software Engineer', 
+    github: require('../../assets/github.png'),
+    linkedin: require('../../assets/linkedin.png'),
+    email: require('../../assets/email.png'),
+    image: require('../../assets/brian.jpg'),
+    bio: 'Brian is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
+  }
+]
