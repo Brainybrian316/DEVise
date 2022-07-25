63be0af9fa68772cb5b9440296f3db4bef16cb81
Brainybrian316 <brainybrian316@gmail.com>
2022-07-25 18:39:58 -0400

    render data of developer

--- src/pages/AboutUs/Developer.js
+++ src/pages/AboutUs/Developer.js
@@ -6,26 +6,27 @@ import CardMedia from '@mui/material/CardMedia';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { GitHub, LinkedIn, Email } from '@mui/icons-material';
-import devDB from './devDB';
+import { devList } from './devDB';
 
 
 
 export default function Developers() {
-  
 
 
   return (
     <>
     
-
-<Card sx={{ display: 'flex' }}>
+{devList.map((dev, index) => (
+<Card sx={{ display: 'flex' }}
+  key={index}
+>
 <Box sx={{ display: 'flex', flexDirection: 'column' }}>
   <CardContent sx={{ flex: '1 0 auto' }}>
     <Typography component="div" variant="h5">
       Software Engineer
     </Typography>
     <Typography variant="subtitle1" color="text.secondary" component="div">
-      Brian M
+      {dev.name}
     </Typography>
   </CardContent>
   <Box sx={{ 
@@ -35,20 +36,30 @@ export default function Developers() {
     pb: 1
    }}>
     <IconButton>
-      <GitHub />
+      <GitHub
+      onClick={() => {
+        window.open(dev.github, '_blank');
+      }}
+       />
     </IconButton>
     <IconButton>
-      <LinkedIn />
+      <LinkedIn
+      onClick={() => {
+        window.open(dev.linkedin, '_blank');
+      }}
+       />
     </IconButton>
     <IconButton>
-      <Email />
+      <Email
+        href={dev.email}
+       />
     </IconButton>
   </Box>
 </Box>
 <CardMedia
   component="img"
   sx={{ width: 151 }}
-  image="https://i.pravatar.cc/300"
+  image={dev.image}
 />
 <Box sx={{ display: 'flex', 
 flexDirection: 'column',
@@ -57,11 +68,11 @@ alignItems: 'center',
  fontSize: '1.5rem'
  }}>
 <Typography variant="subtitle2">
-Brian is a software engineer who is passionate about his craft. He is currently working on a
-project called DEVise.
+  {dev.bio}
 </Typography>
 </Box>
 </Card>
+))}
   
     </>
   );
--- src/pages/AboutUs/devDB.js
+++ src/pages/AboutUs/devDB.js
@@ -1,7 +1,8 @@
 
 
-const devDB = [
+export const devList = [
   {
+    id: 1,
     name: 'Brian M',
     title: 'Software Engineer',
     github: 'https://github.com/Brainybrian316',
@@ -11,15 +12,45 @@ const devDB = [
     bio: 'Brian is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
   },
   {
-    name: 'Brian M',
+    id: 2,
+    name: 'Vince T',
     title: 'Software Engineer',
     github: 'https://github.com/Brainybrian316',
     linkedin:'https://www.linkedin.com/in/brainybrian316/',
     email: 'https://braiybrian316.com/',
     image: 'https://source.unsplash.com/random/200x200?',
-    bio: 'Brian is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
+    bio: 'Vince is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
+  },
+  {
+    id: 3,
+    name: 'VIk M',
+    title: 'Software Engineer',
+    github: 'https://github.com/Brainybrian316',
+    linkedin:'https://www.linkedin.com/in/brainybrian316/',
+    email: 'https://braiybrian316.com/',
+    image: 'https://source.unsplash.com/random/200x200?',
+    bio: 'Vince is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
+  },
+  {
+    id: 4,
+    name: 'Deniro D',
+    title: 'Software Engineer',
+    github: 'https://github.com/Brainybrian316',
+    linkedin:'https://www.linkedin.com/in/brainybrian316/',
+    email: 'https://braiybrian316.com/',
+    image: 'https://source.unsplash.com/random/200x200?',
+    bio: 'Vince is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
+  },
+  {
+    id: 5,
+    name: 'Ethan C',
+    title: 'Software Engineer',
+    github: 'https://github.com/Brainybrian316',
+    linkedin:'https://www.linkedin.com/in/brainybrian316/',
+    email: 'https://braiybrian316.com/',
+    image: 'https://source.unsplash.com/random/200x200?',
+    bio: 'Vince is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
   },
 
 ]
 
-module.export = devDB;
