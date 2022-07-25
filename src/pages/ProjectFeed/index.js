8dda0b75e96ca2d4e1aec965bde9dfe3c49ea8a1
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-25 15:52:32 -0400

    Link project feed pagea and app.js

---
+++ src/pages/ProjectFeed/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import { useTheme } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+
+
+const ProjectFeed = () => {
+    const theme = useTheme();
+    return (
+<>
+<h1>Example Text</h1>
+</>
+
+    );
+  };
+  
+  export default ProjectFeed;
