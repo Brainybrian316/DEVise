92f7c2375334c005b32fe2293c805d6d10dc0660
Brainybrian316 <brainybrian316@gmail.com>
2022-07-25 18:27:19 -0400

    db for developers and about section

--- src/pages/AboutUs/Developer.js
+++ src/pages/AboutUs/Developer.js
@@ -6,58 +6,63 @@ import CardMedia from '@mui/material/CardMedia';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { GitHub, LinkedIn, Email } from '@mui/icons-material';
+import devDB from './devDB';
 
 
 
 export default function Developers() {
+  
+
+
   return (
     <>
-    <Card sx={{ display: 'flex' }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <CardContent sx={{ flex: '1 0 auto' }}>
-          <Typography component="div" variant="h5">
-            Software Engineer
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
-            Brian M
-          </Typography>
-        </CardContent>
-        <Box sx={{ 
-          display: 'flex',
-          justifyContent: 'space-between',
-         alignItems: 'center',
-          pb: 1
-         }}>
-          <IconButton>
-            <GitHub />
-          </IconButton>
-          <IconButton>
-            <LinkedIn />
-          </IconButton>
-          <IconButton>
-            <Email />
-          </IconButton>
-        </Box>
-      </Box>
-      <CardMedia
-        component="img"
-        sx={{ width: 151 }}
-        image="https://i.pravatar.cc/300"
-      />
-      <Box sx={{ display: 'flex', 
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-       fontSize: '1.5rem'
-       }}>
-<Typography variant="subtitle1">
-      Brian is a software engineer who is passionate about his craft. He is currently working on a
-      project called DEVise.
+    
+
+<Card sx={{ display: 'flex' }}>
+<Box sx={{ display: 'flex', flexDirection: 'column' }}>
+  <CardContent sx={{ flex: '1 0 auto' }}>
+    <Typography component="div" variant="h5">
+      Software Engineer
     </Typography>
-    </Box>
-    </Card>
+    <Typography variant="subtitle1" color="text.secondary" component="div">
+      Brian M
+    </Typography>
+  </CardContent>
+  <Box sx={{ 
+    display: 'flex',
+    justifyContent: 'space-between',
+   alignItems: 'center',
+    pb: 1
+   }}>
+    <IconButton>
+      <GitHub />
+    </IconButton>
+    <IconButton>
+      <LinkedIn />
+    </IconButton>
+    <IconButton>
+      <Email />
+    </IconButton>
+  </Box>
+</Box>
+<CardMedia
+  component="img"
+  sx={{ width: 151 }}
+  image="https://i.pravatar.cc/300"
+/>
+<Box sx={{ display: 'flex', 
+flexDirection: 'column',
+justifyContent: 'center',
+alignItems: 'center',
+ fontSize: '1.5rem'
+ }}>
+<Typography variant="subtitle2">
+Brian is a software engineer who is passionate about his craft. He is currently working on a
+project called DEVise.
+</Typography>
+</Box>
+</Card>
   
     </>
   );
 }
-
--- src/pages/AboutUs/devDB.js
+++ src/pages/AboutUs/devDB.js
@@ -1,49 +1,25 @@
 
 
-export const devDB = [
+const devDB = [
   {
     name: 'Brian M',
     title: 'Software Engineer',
-    github: require('../../assets/github.png'),
-    linkedin: require('../../assets/linkedin.png'),
-    email: require('../../assets/email.png'),
-    image: require('../../assets/brian.jpg'),
+    github: 'https://github.com/Brainybrian316',
+    linkedin:'https://www.linkedin.com/in/brainybrian316/',
+    email: 'https://braiybrian316.com/',
+    image: 'https://source.unsplash.com/random/200x200?',
     bio: 'Brian is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
   },
   {
     name: 'Brian M',
-    title: 'Software Engineer', 
-    github: require('../../assets/github.png'),
-    linkedin: require('../../assets/linkedin.png'),
-    email: require('../../assets/email.png'),
-    image: require('../../assets/brian.jpg'),
+    title: 'Software Engineer',
+    github: 'https://github.com/Brainybrian316',
+    linkedin:'https://www.linkedin.com/in/brainybrian316/',
+    email: 'https://braiybrian316.com/',
+    image: 'https://source.unsplash.com/random/200x200?',
     bio: 'Brian is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
   },
-  {
-    name: 'Brian M',
-    title: 'Software Engineer', 
-    github: require('../../assets/github.png'),
-    linkedin: require('../../assets/linkedin.png'),
-    email: require('../../assets/email.png'),
-    image: require('../../assets/brian.jpg'),
-    bio: 'Brian is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
-  },
-  {
-    name: 'Brian M',
-    title: 'Software Engineer', 
-    github: require('../../assets/github.png'),
-    linkedin: require('../../assets/linkedin.png'),
-    email: require('../../assets/email.png'),
-    image: require('../../assets/brian.jpg'),
-    bio: 'Brian is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
-  },
-  {
-    name: 'Brian M',
-    title: 'Software Engineer', 
-    github: require('../../assets/github.png'),
-    linkedin: require('../../assets/linkedin.png'),
-    email: require('../../assets/email.png'),
-    image: require('../../assets/brian.jpg'),
-    bio: 'Brian is a software engineer who is passionate about his craft. He is currently working on a project called DEVise.'
-  }
+
 ]
+
+module.export = devDB;
