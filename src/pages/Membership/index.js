e19996961c454de59a28236bba8d367176d3cd26
DeNiro Dumas <denirod@Deniros-MacBook-Pro.local>
2022-07-25 11:20:38 -0400

    tweaked member page

--- src/pages/Membership/index.js
+++ src/pages/Membership/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import MemberCard from "./MemberCard";
 
+
 function Membership() {
   return (
     <div>
+      <h1 className="memh1">CHOOSE YOUR MEMBERSHIP</h1>
       <MemberCard />
     </div>
   );
