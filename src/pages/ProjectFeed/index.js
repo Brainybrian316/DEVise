07139486354ec10a1812cff03d3084fb34d71e56
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-26 05:40:19 -0400

    Upadte project feed index.js

--- src/pages/ProjectFeed/index.js
+++ src/pages/ProjectFeed/index.js
@@ -16,132 +16,135 @@ import Newsletter from './Newsletter';
 const ProjectFeed = () => {
     const theme = useTheme();
     return (
+        <Box>
         <Box
-        position={'relative'}
-        sx={{
-          backgroundImage:
-            'url("https://assets.maccarianagency.com/backgrounds/img1.jpg")',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          marginTop: -13,
-          paddingTop: 13,
-          '&:after': {
-            position: 'absolute',
-            content: '" "',
-            width: '100%',
-            height: '100%',
-            top: 0,
-            right: 0,
-            bottom: 0,
-            left: 0,
-            zIndex: 1,
-            background: '#161c2d',
-            opacity: 0.6,
-          },
-        }}
-      >
-        <Container
-          zIndex={3}
-          position={'relative'}
-          minHeight={{ xs: 300, sm: 400, md: 600 }}
-          maxHeight={600}
-          display={'flex'}
-          alignItems={'center'}
-          justifyContent={'center'}
+            position={'relative'}
+            sx={{
+            backgroundImage:
+                'url("https://assets.maccarianagency.com/backgrounds/img1.jpg")',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            marginTop: -13,
+            paddingTop: 13,
+            '&:after': {
+                position: 'absolute',
+                content: '" "',
+                width: '100%',
+                height: '100%',
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0,
+                zIndex: 1,
+                background: '#161c2d',
+                opacity: 0.6,
+            },
+            }}
         >
-          <Box>
-            <Box marginBottom={2}>
-              <Typography
-                variant="h2"
-                align={'center'}
-                sx={{
-                  fontWeight: 700,
-                  color: theme.palette.common.white,
-                }}
-              >
-                Project Feed
-              </Typography>
-            </Box>
+            <Container
+            zIndex={3}
+            position={'relative'}
+            minHeight={{ xs: 300, sm: 400, md: 600 }}
+            maxHeight={600}
+            display={'flex'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            >
             <Box>
-              <Typography
-                variant="h6"
-                align={'center'}
-                sx={{
-                  color: theme.palette.common.white,
-                }}
-              >
-                Check out the latest happenings
-              </Typography>
+                <Box marginBottom={2}>
+                <Typography
+                    variant="h2"
+                    align={'center'}
+                    sx={{
+                    fontWeight: 700,
+                    color: theme.palette.common.white,
+                    }}
+                >
+                    Project Feed
+                </Typography>
+                </Box>
+                <Box>
+                <Typography
+                    variant="h6"
+                    align={'center'}
+                    sx={{
+                    color: theme.palette.common.white,
+                    }}
+                >
+                    Check out the latest happenings
+                </Typography>
+                </Box>
             </Box>
-          </Box>
+            </Container>
+            <Box
+            component={'svg'}
+            preserveAspectRatio="none"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            viewBox="0 0 1920 100.1"
+            width={1}
+            maxHeight={120}
+            bottom={0}
+            position={'absolute'}
+            zIndex={2}
+            >
+            <path
+                d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
+            ></path>
+            </Box>
+            <Container
+            sx={{
+                marginTop: '-5rem',
+                position: 'relative',
+                zIndex: 3,
+                paddingY: '0 !important',
+            }}
+            >
+            <SearchBox />
+            </Container>
+            </Box>
+            <Container>
+            <FeaturedArticle />
+            </Container>
+            <Container paddingTop={'0 !important'}>
+            <LastStories />
+            </Container>
+            <Container paddingTop={'0 !important'}>
+            <CaseStudies />
+            </Container>
+            <Box paddingBottom={{ xs: 2, sm: 3, md: 4 }}>
+            <Container paddingTop={'0 !important'}>
+                <PopularArticles />
+            </Container>
+            </Box>
+            <Box
+            component={'svg'}
+            preserveAspectRatio="none"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            viewBox="0 0 1920 100.1"
+            sx={{
+                position: 'absolute',
+                bottom: 0,
+                left: 0,
+                right: 0,
+                transform: 'translateY(50%)',
+                zIndex: 2,
+                width: 1,
+            }}
+            >
+            <path
+                fill={theme.palette.background.paper}
+                d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
+            ></path>
+            </Box>
+            <Container>
+            <Newsletter />
         </Container>
-        <Box
-          component={'svg'}
-          preserveAspectRatio="none"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          viewBox="0 0 1920 100.1"
-          width={1}
-          maxHeight={120}
-          bottom={0}
-          position={'absolute'}
-          zIndex={2}
-        >
-          <path
-            d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
-          ></path>
-        </Box>
-        <Container
-          sx={{
-            marginTop: '-5rem',
-            position: 'relative',
-            zIndex: 3,
-            paddingY: '0 !important',
-          }}
-        >
-          <SearchBox />
-        </Container>
-        <Container>
-          <FeaturedArticle />
-        </Container>
-        <Container paddingTop={'0 !important'}>
-          <LastStories />
-        </Container>
-        <Container paddingTop={'0 !important'}>
-          <CaseStudies />
-        </Container>
-        <Box paddingBottom={{ xs: 2, sm: 3, md: 4 }}>
-          <Container paddingTop={'0 !important'}>
-            <PopularArticles />
-          </Container>
-        </Box>
-        <Box
-          component={'svg'}
-          preserveAspectRatio="none"
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          viewBox="0 0 1920 100.1"
-          sx={{
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            right: 0,
-            transform: 'translateY(50%)',
-            zIndex: 2,
-            width: 1,
-          }}
-        >
-          <path
-            fill={theme.palette.background.paper}
-            d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
-          ></path>
-        </Box>
-        <Container>
-        <Newsletter />
-      </Container>
       </Box>
+      
       
 
       
