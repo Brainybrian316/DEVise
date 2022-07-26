4fb2a981c0ddcad786ec6ae17a0846b14d287be0
Brainybrian316 <brainybrian316@gmail.com>
2022-07-26 11:25:21 -0400

    dynamically create user projects

--- src/pages/UserProjects/index.js
+++ src/pages/UserProjects/index.js
@@ -1,19 +1,17 @@
-import * as React from 'react';
+
 import AspectRatio from '@mui/joy/AspectRatio';
 import { Button, Typography, Box, Card, IconButton } from '@mui/material';
-
 import BookmarkAdd from '@mui/icons-material/BookmarkAdd';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 
+
 export default function UserProjects() {
+
+
+
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        m: 2,
-        p: 2,
-      }}
-    >
+    <Box sx={{ display: 'flex', m: 2,  p: 2, }}>
 
     <Card sx={{ minWidth: '320px' }}>
       <Box sx={{ display: 'flex', justifyContent: 'flex-start'  }}>
@@ -30,15 +28,7 @@ export default function UserProjects() {
         </Typography>
       </Box>
 
-      
-   
-
-      
-
-
-
       <Typography level="body2">April 24 to May 02, 2021</Typography>
-    
 
       <AspectRatio minHeight="120px" maxHeight="200px" sx={{ my: 2 }}>
         <img
