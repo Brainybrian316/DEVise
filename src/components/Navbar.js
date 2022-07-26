4288e346e5d05b91f75bfc233f3d934db4e67043
Brainybrian316 <brainybrian316@gmail.com>
2022-07-26 13:02:09 -0400

    removed dashboard from navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ import LoginBtn from './ui/LoginBtn';
 import { Link } from 'react-router-dom';
 
 // *************variables*****************
-const pages = ['IdeaBin', 'UserProjects', 'Dashboard', 'membership', 'Profile', 'About', 'Contact',];
+const pages = ['IdeaBin', 'UserProjects', 'membership', 'Profile', 'About', 'Contact',];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 
