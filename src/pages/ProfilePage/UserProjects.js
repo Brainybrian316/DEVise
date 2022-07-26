90c63c764e148a3a25dda29c6499a0e375f94c90
Brainybrian316 <brainybrian316@gmail.com>
2022-07-26 01:45:36 -0400

    user projects

--- src/pages/ProfilePage/UserProjects.js
+++ src/pages/ProfilePage/UserProjects.js
@@ -1,4 +1,4 @@
-
+import * as React from 'react';
 import AspectRatio from '@mui/joy/AspectRatio';
 import Box from '@mui/joy/Box';
 import Card from '@mui/joy/Card';
@@ -8,61 +8,105 @@ import IconButton from '@mui/joy/IconButton';
 import Link from '@mui/joy/Link';
 import Favorite from '@mui/icons-material/Favorite';
 
+import { color, Container } from '@mui/system';
+
+import { CreateNewFolder } from '@mui/icons-material';
+import CardCover from '@mui/joy/CardCover';
+
+
 export default function MultipleInteractionCard() {
   return (
-    <Card variant="outlined" sx={{ minWidth: 320 }}>
-      <CardOverflow>
-        <AspectRatio ratio="2">
-          <img
-            src="https://source.unsplash.com/random/320x320"
-            alt=""
-          />
-        </AspectRatio>
-        <IconButton
-          aria-label="Like minimal photography"
-          size="md"
-          variant="solid"
-          color="danger"
-          sx={{
-            position: 'absolute',
-            zIndex: 2,
-            borderRadius: '50%',
-            right: '1rem',
-            bottom: 0,
-            transform: 'translateY(50%)',
-          }}
-        >
-          <Favorite />
-        </IconButton>
-      </CardOverflow>
-      <Typography level="h2" sx={{ fontSize: 'md', mt: 2 }}>
-        <Link href="#multiple-actions" overlay underline="none">
-          Title here
-        </Link>
-      </Typography>
-      <Typography level="body2" sx={{ mt: 0.5, mb: 2 }}>
-        <Link href="#multiple-actions">Summary here</Link>
-      </Typography>
-      <CardOverflow
-        variant="soft"
+    <Container maxWidth="lg">
+      <Box 
         sx={{
           display: 'flex',
-          gap: 1.5,
-          py: 1.5,
-          px: 'var(--Card-padding)',
-          borderTop: '1px solid',
-          borderColor: 'neutral.outlinedBorder',
-          bgcolor: 'background.level1',
-        }}
-      >
-        <Typography level="body3" sx={{ fontWeight: 'md', color: 'text.secondary' }}>
-          6.3k Likes/Reviews here
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}>
+    <Card variant="outlined" sx={{ minWidth: 320, maxWidth: 750,  }}>
+   
+      <Box sx={{ position: 'relative' }}>
+        <AspectRatio ratio="4/3">
+          <figure>
+            <img
+              alt="Yosemite by Casey Horner"
+              width="330"
+              height="247"
+              sizes="338px"
+              data-sizes="auto"
+              src="https://source.unsplash.com/random/300"
+            />
+          </figure>
+        </AspectRatio>
+        <CardCover
+          className="gradient-cover"
+          sx={{
+            '&:hover, &:focus-within': {
+              opacity: 1,
+            },
+            opacity: 0,
+            transition: '0.1s ease-in',
+            background: 'linear-gradient(to bottom, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.7) 100%)',
+          }}
+        >
+          {/* The first box acts as a container that inherits style from the CardCover */}
+          <Box>
+            <Box
+              sx={{
+                p: 2,
+                display: 'flex',
+                alignItems: 'center',
+                gap: 1.5,
+                flexGrow: 1,
+                alignSelf: 'flex-end',
+              }}
+            >
+              <Typography level="h2" noWrap sx={{ fontSize: 'lg' }}>
+                <Link
+                  href="#dribbble-shot"
+                  overlay
+                  underline="none"
+                  sx={{
+                    color: '#fff',
+                    textOverflow: 'ellipsis',
+                    overflow: 'hidden',
+                    display: 'block',
+                  }}
+                >
+                  Project Title
+                </Link>
+              </Typography>
+              <IconButton size="sm" sx={{ 
+                ml: 'auto',
+                color: '#fff',
+                 }}>
+                <CreateNewFolder />
+              </IconButton>
+              <IconButton size="sm" sx={{ 
+                color: '#fff',
+                 }}>
+                <Favorite />
+              </IconButton>
+            </Box>
+          </Box>
+        </CardCover>
+      </Box>
+      <Typography level="h3" noWrap sx={{ 
+        pt: 2,
+        fontSize: 'lg' }}>
+      Summary:
+      </Typography>
+      <CardOverflow>
+        <Typography level="paragraph" noWrap sx={{
+          fontSize: 'sm',
+          lineHeight: 'lg',
+        }}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         </Typography>
-        <Box sx={{ width: 2, bgcolor: 'divider' }} />
-        <Typography level="body3" sx={{ fontWeight: 'md', color: 'text.secondary' }}>
-          Languages here
-        </Typography>
+
       </CardOverflow>
     </Card>
+    </Box>
+    </Container>
   );
 }
