b694fa15e18331e20bdaef7664a3b8055d806d37
cobalt88 <vincentteune@gmail.com>
2022-07-25 20:40:04 -0400

    Feature About Us

---
+++ src/pages/AboutUs/Notes.js
@@ -0,0 +1,29 @@
+<div className="founderCard">
+  <div className="FounderName">
+    <h3>Founder 1</h3>
+  </div>
+
+  <div className="founderImg">
+    <Avatar
+      alt="Remy Sharp"
+      src={roboAvatars1}
+      sx={{ width: 150, height: 150 }}
+    />
+  </div>
+
+  <div className="founderInfo">
+    <p>Info goes here, maybe a very brief summary of each founder.</p>
+  </div>
+
+  <div className="founderLinks">
+    <a href="#">
+      <Avatar alt="LinkedIn" src={linkedinColor} />
+    </a>
+    <a href="#">
+      <Avatar alt="Github" src={github1} />
+    </a>
+    <a href="#">
+      <Avatar alt="Portfolio" src={gearBrain} />
+    </a>
+  </div>
+</div>;
