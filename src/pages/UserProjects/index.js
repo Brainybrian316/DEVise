2afcc44c927c38e40c5ae5e5b3c2b470fcf821bc
Brainybrian316 <brainybrian316@gmail.com>
2022-07-26 11:37:14 -0400

    projects card display database

--- src/pages/UserProjects/index.js
+++ src/pages/UserProjects/index.js
@@ -3,20 +3,28 @@ import AspectRatio from '@mui/joy/AspectRatio';
 import { Button, Typography, Box, Card, IconButton } from '@mui/material';
 import BookmarkAdd from '@mui/icons-material/BookmarkAdd';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import { useGetAllUserProjectsQuery } from '../../hooks/Queries';
 
 
 export default function UserProjects() {
+
+  const { userProjects, loading, error } = useGetAllUserProjectsQuery();
+
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error</p>;
+  
 
 
 
 
   return (
-    <Box sx={{ display: 'flex', m: 2,  p: 2, }}>
-
-    <Card sx={{ minWidth: '320px' }}>
+    <Box sx={{ display: 'flex', m: 2,  p: 2, justifyContent: 'space-evenly', flexWrap: 'wrap', flexDirection: 'row'   }}>
+    {userProjects.map(userProject => (
+    <Card   key={userProject.id}sx={{ minWidth: '450px', maxWidth: '300px' }}>
       <Box sx={{ display: 'flex', justifyContent: 'flex-start'  }}>
         <Typography variant='h6'>
-          Yosemite National Park
+          {userProject.title}
           <IconButton
           sx={{ ml: 3 }}
           >
@@ -28,7 +36,9 @@ export default function UserProjects() {
         </Typography>
       </Box>
 
-      <Typography level="body2">April 24 to May 02, 2021</Typography>
+      <Typography level="body2">
+        {userProject.summary}
+      </Typography>
 
       <AspectRatio minHeight="120px" maxHeight="200px" sx={{ my: 2 }}>
         <img
@@ -38,7 +48,8 @@ export default function UserProjects() {
       </AspectRatio>
       <Box sx={{ display: 'flex' }}>
         <div>
-          <Typography level="body3">Total price: $1,000</Typography>
+          <Typography level="body3">Total price:  {userProject.price}
+          </Typography>
         </div>
    
           <Button
@@ -50,6 +61,8 @@ export default function UserProjects() {
         </Button>
       </Box>
     </Card>
+    ))}
     </Box>
+    
   );
 }
