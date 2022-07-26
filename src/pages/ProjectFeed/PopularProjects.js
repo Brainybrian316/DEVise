4c771bcdeb710b308fe44c30613de8a0eaa08d78
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-26 07:18:04 -0400

    Update popular projects layout

--- src/pages/ProjectFeed/PopularProjects.js
+++ src/pages/ProjectFeed/PopularProjects.js
@@ -82,75 +82,32 @@ const PopularProjects = () => {
   return (
     <Box>
       <Box
-        padding={2}
-        width={1}
-        component={Card}
-        boxShadow={4}
+        display={'flex'}
+        justifyContent={'space-between'}
+        alignItems={{ xs: 'flex-start', sm: 'center' }}
+        flexDirection={{ xs: 'column', sm: 'row' }}
         marginBottom={4}
       >
-        <form noValidate autoComplete="off">
-          <Box display="flex" alignItems={'center'}>
-            <Box width={1} marginRight={1}>
-              <TextField
-                sx={{
-                  height: 54,
-                  '& .MuiOutlinedInput-notchedOutline': {
-                    border: '0 !important',
-                  },
-                }}
-                variant="outlined"
-                color="primary"
-                size="medium"
-                placeholder="Search an article"
-                fullWidth
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <Box
-                        component={'svg'}
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                        width={24}
-                        height={24}
-                        color={'primary.main'}
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                        />
-                      </Box>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            </Box>
-            <Box display={{ xs: 'none', sm: 'block' }} marginRight={2}>
-              <Typography
-                color={'text.secondary'}
-                variant={'subtitle2'}
-                sx={{ whiteSpace: 'nowrap' }}
-              >
-                123 Results
-              </Typography>
-            </Box>
-            <Box>
-              <Button
-                sx={{ height: 54, minWidth: 100, whiteSpace: 'nowrap' }}
-                variant="contained"
-                color="primary"
-                size="medium"
-                fullWidth
-              >
-                Search
-              </Button>
-            </Box>
+        <Box>
+          <Typography fontWeight={700} variant={'h6'} gutterBottom>
+            Popular articles
+          </Typography>
+          <Typography color={'text.secondary'}>
+            Here’s what we’ve been up to recently.
+          </Typography>
+        </Box>
+        <Box display="flex" marginTop={{ xs: 2, md: 0 }}>
+          <Box
+            component={Button}
+            variant="outlined"
+            color="primary"
+            size="large"
+            marginLeft={2}
+          >
+            View all
           </Box>
-        </form>
-      </Box>
+        </Box>
+      </Box>   
       <Grid container spacing={4}>
         {mock.map((item, i) => (
           <Grid item xs={12} sm={6} md={4} key={i}>
