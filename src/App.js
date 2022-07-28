3b4dea2746750b4999843ad0ba1b406954a9dafd
Brainybrian316 <brainybrian316@gmail.com>
2022-07-28 14:37:21 -0400

    working on heroku

--- src/App.js
+++ src/App.js
@@ -17,8 +17,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@ap
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
-  // uri: '/graphql',
+  uri: '/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
