4f0b7542790b46110023daa62db3a45e2a24edfd
Brainybrian316 <brainybrian316@gmail.com>
2022-07-28 13:35:39 -0400

    working on heroku

--- src/services/server.js
+++ src/services/server.js
@@ -33,11 +33,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../build')));
+  app.use(express.static(path.join(__dirname, '../../build')));
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
+  res.sendFile(path.join(__dirname, '../../build/index.html'));
   
 });
 
