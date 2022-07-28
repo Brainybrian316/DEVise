906fe92ef799a7727aacb0f0b5bf6646093aa222
Vikaash Maharaj <vikaashmaharaj@gmail.com>
2022-07-27 23:53:57 -0400

    Remove home page button and add temp text

--- src/pages/Home/Header.jsx
+++ src/pages/Home/Header.jsx
@@ -11,11 +11,23 @@ function Header() {
           <span>Welcome to DEVISE</span>
         </h1>
         <p className="details">
-          Welcome to DEVISE where your ideas turn into collaboration!
+          Have an idea? Turn it into a collaboration!
         </p>
-        <a href="#" className="cv-btn">
+        <br></br>
+        <p className="details">
+          Get your idea out of your head and into the world. Share it with a trusted community that knows how to stick handle it.
+        </p>
+        <br></br>
+        <p className="details">
+          Once it’s live, our community can share their honest feedback with comments, up-votes and questions.
+        </p>
+        <br></br>
+        <p className="details">
+          Building can be lonely. Amplify your efforts with people that share the same passion for your idea, and start building today!
+        </p>
+        {/* <a href="#" className="cv-btn">
           Download
-        </a>
+        </a> */}
       </div>
     </div>
   );
